ff6799b797d48cbf5ac870c830eafe20416b3a5d
‘张磊’ <1196357794@qq.com>
2025-02-08 19:38:56 +0800

    用UnityTimer实现

--- Assets/脚本/坐标随机变化.cs
+++ Assets/脚本/坐标随机变化.cs
@@ -1,55 +1,81 @@
 using UnityEngine;
+using UnityTimer;
 
-public class Spheregrid : MonoBehaviour
+public class SphereGrid : MonoBehaviour
 {
-    public GameObject sphereprefab; // Sphere预制体
+    public GameObject spherePrefab; // Sphere预制体
     public float space = 1.0f; // 球体之间的间隔
+    public Vector2 gridSize = new Vector2(5, 5); // 网格尺寸
+    public Vector2 randomRange = new Vector2(0f, 5f); // 随机位置范围
 
-    private GameObject[,] spheres; // 用于存储所有Sphere实例的二维数组
+    private GameObject[,] spheres; // 存储所有Sphere实例
+    private Timer positionTimer;   // 位置更新计时器
+    private Timer logTimer;        // 日志输出计时器
 
-    void Start()
+    void Awake()
     {
-        generategrid();
-        InvokeRepeating("UpdateSpheres", 0, 0.5f); // 每隔0.5秒调用一次UpdateSpheres方法
+        GenerateGrid();
+
+        // 注册位置更新计时器（每秒执行）
+        positionTimer = Timer.Register(1f, UpdatePositions,
+            isLooped: true);
+
+        // 注册日志输出计时器（每0.5秒执行）
+        logTimer = Timer.Register(0.5f, LogPositions,
+            isLooped: true);
     }
 
-    void generategrid()
+    void OnDestroy()
     {
-        spheres = new GameObject[5, 5]; // 创建一个5x5的数组来存储Sphere实例
+        Timer.Cancel(positionTimer);
+        Timer.Cancel(logTimer);
+    }
 
-        for (int x = 0; x < 5; x++)
+    void GenerateGrid()
+    {
+        int width = (int)gridSize.x;
+        int height = (int)gridSize.y;
+        spheres = new GameObject[width, height];
+
+        for (int x = 0; x < width; x++)
         {
-            for (int z = 0; z < 5; z++)
+            for (int z = 0; z < height; z++)
             {
                 Vector3 position = new Vector3(x * space, 0, z * space);
-                GameObject sphere = Instantiate(sphereprefab, position, Quaternion.identity);
-                spheres[x, z] = sphere; // 将Sphere实例存储在数组中
+                GameObject sphere = Instantiate(spherePrefab, position, Quaternion.identity);
+                sphere.name = $"Sphere_{x}_{z}"; // 设置唯一名称
+                sphere.transform.SetParent(transform); // 设为子物体方便管理
+                spheres[x, z] = sphere;
             }
         }
     }
 
-    void UpdateSpheres()
+    void UpdatePositions()
     {
-        for (int x = 0; x < 5; x++)
+        for (int x = 0; x < gridSize.x; x++)
         {
-            for (int z = 0; z < 5; z++)
+            for (int z = 0; z < gridSize.y; z++)
             {
-                if (spheres[x, z] != null)
-                {
-                    // 更新Sphere的名称
-                    spheres[x, z].name = "Sphere_" + Random.Range(0, 1000);
+                if (spheres[x, z] == null) continue;
 
-                    // 更新Sphere的位置
-                    Vector3 newPosition = new Vector3(
-                        Random.Range(0, 5) * space,
-                        0,
-                        Random.Range(0, 5) * space
-                    );
-                    spheres[x, z].transform.position = newPosition;
+                // 生成连续随机位置
+                float newX = Random.Range(randomRange.x, randomRange.y) * space;
+                float newZ = Random.Range(randomRange.x, randomRange.y) * space;
+                spheres[x, z].transform.position = new Vector3(newX, 0, newZ);
+            }
+        }
+    }
 
-                    // 输出Sphere的名称和位置
-                    Debug.Log("Sphere Name: " + spheres[x, z].name + ", Position: " + newPosition);
-                }
+    void LogPositions()
+    {
+        for (int x = 0; x < gridSize.x; x++)
+        {
+            for (int z = 0; z < gridSize.y; z++)
+            {
+                if (spheres[x, z] == null) continue;
+
+                Debug.Log($"{spheres[x, z].name} - " +
+                          $"Position: {spheres[x, z].transform.position.ToString("F2")}");
             }
         }
     }
